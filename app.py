380a8a06d116902f0dc370df6f1351838e4eaf8a
Priyabrata Pati <priyabrata.pati36@gmail.com>
2019-05-02 22:28:21 +0530

    Progress in boilerplate

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
-from flask import Flask, request, Response, jsonify
+from flask import Flask, request, Response, jsonify, send_from_directory
 from flask_restful import Resource, Api
 from json import dumps
 import json
 from flask_cors import CORS
 
 # Initialize app and enable cors to it
-app = Flask(__name__)
+app = Flask(__name__, static_folder="public")
 CORS(app)
 api = Api(app)
 
 # Register all the folder structures inside src and their path
 import sys
 import os
-
 project_root = os.path.dirname(os.path.realpath(os.path.join(__file__, '.')))
 sys.path.append(os.path.join(project_root, 'src', 'controller'))
 sys.path.append(os.path.join(project_root, 'src', 'service'))
@@ -21,6 +20,10 @@ sys.path.append(os.path.join(project_root, 'src', 'utilities'))
 
 # Database connection initiate
 import database_config
+
+@app.route('/')
+def serve_page():
+    return send_from_directory('public', 'index.html')
 
 # App controller REST routes
 import app_controller
