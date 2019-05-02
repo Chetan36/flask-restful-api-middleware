d387979f84d8f5186467722a6d44e102bf69d2a2
Priyabrata Pati <priyabrata.pati36@gmail.com>
2019-05-02 23:37:58 +0530

    User CRUD complete

--- src/repository/user_repository.py
+++ src/repository/user_repository.py
@@ -1,6 +1,7 @@
 import json
 from bson.json_util import dumps
 from bson import ObjectId
+import dateutil
 import datetime
 import copy
 
@@ -40,10 +41,15 @@ def create(user):
 def update(user_id, user):
     try:
         db_user = json.loads(dumps(user_collection.find_one({"id": int(user_id)})))
-        final_document = copy.deepcopy(db_user)
-        user["id"] = final_document["id"]
-        user["updatedAt"] = datetime.datetime.now()
-        user_collection.update({"id": int(user_id)}, user, upsert=True)
-        return (json.loads(dumps(user)))
+        updated_user = {
+            "id": db_user["id"],
+            "name": user["name"],
+            "email": user["email"],
+            "password": user["password"],
+            "createdAt": datetime.datetime.fromtimestamp(int(db_user["createdAt"]["$date"]) / 1000),
+            "updatedAt": datetime.datetime.now()
+        }
+        user_collection.update({"id": int(user_id)}, updated_user, upsert=True)
+        return (json.loads(dumps(updated_user)))
     except:
         print("Error happened")
